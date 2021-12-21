d79f9a9e30f5c96ad4cd6d3903d231fef5d0e127
ahn <kiki_0001@naver.com>
2021-12-21 22:59:07 +0900

    header file add

---
+++ push_swap.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heahn <heahn@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 23:51:16 by heahn             #+#    #+#             */
+/*   Updated: 2021/12/19 23:51:35 by heahn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+
+typedef struct s_stack
+{
+	
+}				t_stack;
+
+
+
+
+
+#endif
