04e5caded3f26b1273cc9497938b97ffb361490a
ahn <kiki_0001@naver.com>
2022-01-20 19:56:52 +0900

    1st commit

--- push_swap.c
+++ push_swap.c
@@ -12,15 +12,314 @@
 
 #include "push_swap.h"
 
+void	ft_error(void)
+{
+	write(2, "Error\n", 6);
+	exit(1);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	if (fd > 0)
+	{
+		write(fd, s, sizeof(char) * ft_strlen(s));
+		write(fd, "\n", 1);
+	}
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+		*((unsigned char *)s + i++) = '\0';
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	unsigned int i;
+
+	if (!dest || !src)
+		return (0);
+	i = 0;
+	while (src[i] && size != 0 && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size != 0)
+		dest[i] = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
+
+// ' ' 공백의 수 카운트  리턴값= ' ' + 1
+static int		str_size(const char *str, char c)
+{
+	size_t	size;
+	int		flag;
+
+	size = 0;
+	flag = 1;
+	while (*str)
+	{
+		if (flag && *str != c)  //flag가 1 이고 str=' ' 아니면 
+		{
+			size++;
+			flag = 0;
+		}
+		if (*str == c)
+			flag = 1;
+		str++;
+	}
+	return (size);
+}
+
+char	**ft_free(char **tmp, size_t idx)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < idx)
+		free(tmp[i++]);
+	free(tmp);
+	return (0);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**ret;
+	char	*tmp;
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	if (!(ret = (char **)malloc(sizeof(char *) * (str_size(s, c) + 1) + 1)))
+		return (0);
+	while (*s)
+	{
+		if (*s == c)
+		{
+			s++;
+			continue;
+		}
+		tmp = (char *)s;
+		while (*s && *s != c)
+			s++;
+		if (!(ret[i] = (char *)malloc(s - tmp + 1)))
+			return (ft_free(ret, i));
+		ft_strlcpy(ret[i++], tmp, s - tmp + 1);
+	}
+	ret[i] = 0;
+	return (ret);
+}
+
+static int	str_parser(int argc, char **argv)
+{
+	int		result;
+	char	**temp;
+	int		i;
+
+	result = 0;
+	while (argc-- > 1)
+	{
+		i = 0;
+		temp = ft_split(*(++argv), ' ');
+		if (!temp)
+			exit(EXIT_FAILURE);
+		while (temp[i] != 0)
+		{
+			result++;
+			free(temp[i]);
+			i++;
+		}
+		free(temp);
+	}
+	return (result);
+}
+
+char	**argv_parser(int argc, char **argv)
+{
+	char	**arguments;
+	char	**temp;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	arguments = (char **)malloc(sizeof(char *) * (str_parser(argc, argv) + 1));
+	if (!arguments)
+		ft_error();
+	while (argc-- > 1)
+	{
+		i = 0;
+		temp = ft_split(*(++argv), ' ');
+		if (!temp)
+			ft_error();
+		while (temp[i] != 0)
+			arguments[j++] = temp[i++];
+		free(temp);
+	}
+	arguments[j] = 0;
+	return (arguments);
+}
+
+static int	ft_integer_check(const char *str)
+{
+	int		i;
+	ssize_t	result;
+	int		sign;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i++] == '-')
+			sign = -1;
+	}
+	if (str[i] == 0)
+		ft_error();
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			ft_error();
+		result = (result * 10) + str[i] - '0';
+		i++;
+	}
+	result *= sign;
+	if (ft_strlen(str) > 11 || result > 2147483647 || result < -2147483648)
+		ft_error();
+	return ((int)result);
+}
+
+void	duplication_check(int len, int *num)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < len)
+	{
+		j = i + 1;
+		while (j < len)
+		{
+			if (num[i] == num[j])
+				ft_error();
+			j++;
+		}
+		i++;
+	}
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (1)
+	{
+		if ((s1[i] != s2[i]) || !s1[i] || !s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+}
+//스택 길이
+int	stack_len(t_stack *a)
+{
+	int		i;
+	int		start;
+	t_stack	*ptr;
+
+	if (!a)
+		return (0);
+	i = 1;
+	ptr = a;
+	start = ptr->data;
+	while (ptr->next->data != start)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
+
+static t_stack	*add_element(int data)
+{
+	t_stack	*temp;
+
+	temp = (t_stack *)malloc(sizeof(t_stack));
+	if (!temp)
+		ft_error();
+	temp->data = data;
+	temp->flag = 0;
+	temp->max_flag = 0;
+	temp->prev = temp;
+	temp->next = temp;
+	return (temp);
+}
+
+void	stack_init(t_stack **a, int len, int *arguments)
+{
+	t_stack	*new;
+
+	*a = add_element(arguments[len - 1]);
+	while (len > 1)
+	{
+		new = add_element(arguments[len - 2]);
+		new->next = *a;
+		new->prev = (*a)->prev;
+		(*a)->prev->next = new;
+		(*a)->prev = new;
+		(*a) = new;
+		len--;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	t_stack *s_a;
-	t_stack *s_b;
+	t_stack	*a;
+	int		len;
+	int		*num;
+	char	**arg;
 
-	if (argc == 1)
-		exit(1);
-	s_a = 0;
-	s_b = 0;
-	
+	a = 0;
+	arg = argv_parser(argc, argv);    //arguments에 인수로 받은 각 값을 넣음
+/*
+    int i =0;
+    while(arguments[i])
+    {   
+        printf(" : %s \n", arguments[i]);
+        i++;
+    }
+*/
+	len = ft_int_check_return_len(&num, arg);      //integer인지, int 범위인지 체크하고 갯수 받아옴, num에는int형 인자 argument에는 char형 인자
+	duplication_check(len, num);            
+	stack_init(&a, len, num);
+	//stack_sort(&a, len);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -14,18 +14,32 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+//# include "libft.h"
 # include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
 
+char	**argv_parser(int argc, char **argv);
+void	duplication_check(int argc, int *argv);
+int		ft_strcmp(const char *s1, const char *s2);
+static int	ft_integer_check(const char *str);
 
 typedef struct s_stack
 {
-	
-}				t_stack;
+	int				data;
+	struct s_stack	*prev;
+	struct s_stack	*next;
 
+	int				max_flag;
+	int				flag;
+}					t_stack;
 
+typedef struct s_all
+{
+	t_stack			*stack;
+	t_stack			*last;
+}					t_all;
 
-
+void	stack_init(t_stack **lst, int len, int *arguments);
+int		stack_len(t_stack *a);
 
 #endif
